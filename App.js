3e456f99a1075c9588ca0e1169675ecc1447357c
nhn2004 <njcevall@espol.edu.ec>
2026-05-31 13:19:16 -0500

    Presentacion de Login

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation';
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="light" />
+    <SafeAreaProvider>
+      <StatusBar style="dark" />
       <RootNavigator />
-    </>
+    </SafeAreaProvider>
   );
 }
